fd631784baaaeefbed4ac57b275f7d3b5d2e7c76
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2023-03-23 19:11:24 -0300

    update commit

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -12,9 +12,8 @@ int main(){
     punt1=&vt[0];
     punt2=&vt[N];
     while(punt1!=punt2){
-    	vt[i]=1+rand()%100;
-    	printf("%f \n",vt[i]);
-		i++;
+    	*punt1=1+rand()%100;
+    	printf("%f \n",*punt1);
     	punt1++;
 	}
     return 0;
